89bed36efee698c5ce193715768aed3a3b335453
Muhammad Farhan Aslam <farhanmehar422538@gmail.com>
2024-10-01 17:26:54 +0500

    Update validator.py

--- neurons/validator.py
+++ neurons/validator.py
@@ -1,32 +1,131 @@
+# # import os
+# # import sys
+# # import asyncio
+# # import datetime as dt
+# # import wandb
+# # import bittensor as bt
+
+# # # Set the project root path
+# # project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+# # audio_subnet_path = os.path.abspath(project_root)
+
+# # # Add the project root and 'AudioSubnet' directories to sys.path
+# # sys.path.insert(0, project_root)
+# # sys.path.insert(0, audio_subnet_path)
+
+# # from ttm.ttm import MusicGenerationService
+# # from ttm.aimodel import AIModelService
+
+
+# # class AIModelController:
+# #     def __init__(self):
+# #         self.aimodel = AIModelService()
+# #         self.music_generation_service = MusicGenerationService()
+# #         self.last_run_start_time = dt.datetime.now()
+
+# #     async def run_services(self):
+# #         while True:
+# #             self.check_and_update_wandb_run()
+# #             await self.music_generation_service.run_async()
+
+# #     def check_and_update_wandb_run(self):
+# #         # Calculate the time difference between now and the last run start time
+# #         current_time = dt.datetime.now()
+# #         time_diff = current_time - self.last_run_start_time
+# #         # Check if 4 hours have passed since the last run start time
+# #         if time_diff.total_seconds() >= 4 * 3600:  # 4 hours * 3600 seconds/hour
+# #             self.last_run_start_time = current_time  # Update the last run start time to now
+# #             if self.wandb_run:
+# #                 wandb.finish()  # End the current run
+# #             self.new_wandb_run()  # Start a new run
+
+# #     def new_wandb_run(self):
+# #         now = dt.datetime.now()
+# #         run_id = now.strftime("%Y-%m-%d_%H-%M-%S")
+# #         name = f"Validator-{self.aimodel.uid}-{run_id}"
+# #         commit = self.aimodel.get_git_commit_hash()
+# #         self.wandb_run = wandb.init(
+# #             name=name,
+# #             project="AudioSubnet_Valid",
+# #             entity="subnet16team",
+# #             config={
+# #                 "uid": self.aimodel.uid,
+# #                 "hotkey": self.aimodel.wallet.hotkey.ss58_address,
+# #                 "run_name": run_id,
+# #                 "type": "Validator",
+# #                 "tao (stake)": self.aimodel.metagraph.neurons[self.aimodel.uid].stake.tao,
+# #                 "commit": commit,
+# #             },
+# #             tags=self.aimodel.sys_info,
+# #             allow_val_change=True,
+# #             anonymous="allow",
+# #         )
+# #         bt.logging.debug(f"Started a new wandb run: {name}")
+
+
+# # async def main():
+# #     controller = AIModelController()
+# #     controller.new_wandb_run()
+# #     await controller.run_services()
+
+
+# # if __name__ == "__main__":
+# #     asyncio.run(main())
+
+
+# ###################-------------------------------With API------------------------------###############################
 # import os
 # import sys
 # import asyncio
 # import datetime as dt
 # import wandb
 # import bittensor as bt
+# import uvicorn
+# from pyngrok import ngrok  # Import ngrok from pyngrok
 
 # # Set the project root path
 # project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+# # Set the 'AudioSubnet' directory path
 # audio_subnet_path = os.path.abspath(project_root)
 
 # # Add the project root and 'AudioSubnet' directories to sys.path
 # sys.path.insert(0, project_root)
 # sys.path.insert(0, audio_subnet_path)
 
+# from lib.globals import service_flags
 # from ttm.ttm import MusicGenerationService
 # from ttm.aimodel import AIModelService
 
+# # Check if the 'app' folder exists
+# if os.path.exists(os.path.join(project_root, 'app')):
+#     from app.fastapi_server import create_app
 
-# class AIModelController:
+
+# class AIModelController():
 #     def __init__(self):
 #         self.aimodel = AIModelService()
 #         self.music_generation_service = MusicGenerationService()
+#         self.current_service = self.music_generation_service
+#         self.service = service_flags
 #         self.last_run_start_time = dt.datetime.now()
+
+#     async def run_fastapi_with_ngrok(self, app):
+#         # Setup ngrok tunnel
+#         ngrok_tunnel = ngrok.connect(38287, bind_tls=True)
+#         print('Public URL:', ngrok_tunnel.public_url)
+#         # Create and start the uvicorn server as a background task
+#         config = uvicorn.Config(app=app, host="0.0.0.0", port=38287)  # Ensure port matches ngrok's
+#         server = uvicorn.Server(config)
+#         # No need to await here, as we want this to run in the background
+#         task = asyncio.create_task(server.serve())
+#         return ngrok_tunnel, task  # Returning task if you need to cancel it later
+
 
 #     async def run_services(self):
 #         while True:
 #             self.check_and_update_wandb_run()
-#             await self.music_generation_service.run_async()
+#             if isinstance(self.current_service, MusicGenerationService) and self.service["MusicGenerationService"]:
+#                 await self.current_service.run_async()
 
 #     def check_and_update_wandb_run(self):
 #         # Calculate the time difference between now and the last run start time
@@ -62,18 +161,37 @@
 #         )
 #         bt.logging.debug(f"Started a new wandb run: {name}")
 
+# async def setup_and_run(controller):
+#     tasks = []
+#     if os.path.exists(os.path.join(project_root, 'app')):
+#         secret_key = os.getenv("AUTH_SECRET_KEY")
+#         if not secret_key:
+#             raise ValueError("Auth Secret key not found in environment variable AUTH_SECRET_KEY")
+#         app = create_app(secret_key)
+#         # Start FastAPI with ngrok without blocking
+#         ngrok_tunnel, server_task = await controller.run_fastapi_with_ngrok(app)
+#         tasks.append(server_task)  # Keep track of the server task if you need to cancel it later
+    
+#     # Start service-related tasks
+#     service_task = asyncio.create_task(controller.run_services())
+#     tasks.append(service_task)
+
+#     # Wait for all tasks to complete
+#     await asyncio.gather(*tasks)
+
+#     # Cleanup, in case you need to close ngrok or other resources
+#     ngrok_tunnel.close()
 
 # async def main():
 #     controller = AIModelController()
-#     controller.new_wandb_run()
-#     await controller.run_services()
-
+#     await setup_and_run(controller)
 
 # if __name__ == "__main__":
 #     asyncio.run(main())
 
 
-###################-------------------------------With API------------------------------###############################
+
+#-----------------------------------------------------------------------------------------------------------#
 import os
 import sys
 import asyncio
@@ -81,11 +199,10 @@ import datetime as dt
 import wandb
 import bittensor as bt
 import uvicorn
-from pyngrok import ngrok  # Import ngrok from pyngrok
+from pyngrok import ngrok
 
 # Set the project root path
 project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
-# Set the 'AudioSubnet' directory path
 audio_subnet_path = os.path.abspath(project_root)
 
 # Add the project root and 'AudioSubnet' directories to sys.path
@@ -101,25 +218,25 @@ if os.path.exists(os.path.join(project_root, 'app')):
     from app.fastapi_server import create_app
 
 
-class AIModelController():
+class AIModelController:
     def __init__(self):
         self.aimodel = AIModelService()
         self.music_generation_service = MusicGenerationService()
         self.current_service = self.music_generation_service
         self.service = service_flags
         self.last_run_start_time = dt.datetime.now()
+        self.wandb_run = None  # Initialize wandb_run
 
     async def run_fastapi_with_ngrok(self, app):
         # Setup ngrok tunnel
         ngrok_tunnel = ngrok.connect(38287, bind_tls=True)
         print('Public URL:', ngrok_tunnel.public_url)
         # Create and start the uvicorn server as a background task
-        config = uvicorn.Config(app=app, host="0.0.0.0", port=38287)  # Ensure port matches ngrok's
+        config = uvicorn.Config(app=app, host="0.0.0.0", port=38287)
         server = uvicorn.Server(config)
-        # No need to await here, as we want this to run in the background
+        # Start server as a background task
         task = asyncio.create_task(server.serve())
-        return ngrok_tunnel, task  # Returning task if you need to cancel it later
-
+        return ngrok_tunnel, task
 
     async def run_services(self):
         while True:
@@ -128,15 +245,13 @@ class AIModelController():
                 await self.current_service.run_async()
 
     def check_and_update_wandb_run(self):
-        # Calculate the time difference between now and the last run start time
         current_time = dt.datetime.now()
         time_diff = current_time - self.last_run_start_time
-        # Check if 4 hours have passed since the last run start time
         if time_diff.total_seconds() >= 4 * 3600:  # 4 hours * 3600 seconds/hour
-            self.last_run_start_time = current_time  # Update the last run start time to now
+            self.last_run_start_time = current_time
             if self.wandb_run:
-                wandb.finish()  # End the current run
-            self.new_wandb_run()  # Start a new run
+                wandb.finish()
+            self.new_wandb_run()
 
     def new_wandb_run(self):
         now = dt.datetime.now()
@@ -161,30 +276,38 @@ class AIModelController():
         )
         bt.logging.debug(f"Started a new wandb run: {name}")
 
-async def setup_and_run(controller):
+async def setup_and_run(controller, with_api):
     tasks = []
-    if os.path.exists(os.path.join(project_root, 'app')):
+    if with_api and os.path.exists(os.path.join(project_root, 'app')):
         secret_key = os.getenv("AUTH_SECRET_KEY")
         if not secret_key:
             raise ValueError("Auth Secret key not found in environment variable AUTH_SECRET_KEY")
         app = create_app(secret_key)
-        # Start FastAPI with ngrok without blocking
         ngrok_tunnel, server_task = await controller.run_fastapi_with_ngrok(app)
-        tasks.append(server_task)  # Keep track of the server task if you need to cancel it later
-    
-    # Start service-related tasks
+        tasks.append(server_task)
+
     service_task = asyncio.create_task(controller.run_services())
     tasks.append(service_task)
 
-    # Wait for all tasks to complete
     await asyncio.gather(*tasks)
-
-    # Cleanup, in case you need to close ngrok or other resources
-    ngrok_tunnel.close()
+    if with_api:
+        ngrok_tunnel.close()
 
 async def main():
     controller = AIModelController()
-    await setup_and_run(controller)
+
+    # Ask the user if they want to start with or without API
+    choice = input("Do you want to start the validator with API? (yes/no): ").strip().lower()
+
+    if choice == 'yes':
+        with_api = True
+        if not os.getenv("AUTH_SECRET_KEY"):
+            secret_key_input = input("Please provide your AUTH_SECRET_KEY: ").strip()
+            os.environ["AUTH_SECRET_KEY"] = secret_key_input
+    else:
+        with_api = False
+
+    await setup_and_run(controller, with_api)
 
 if __name__ == "__main__":
     asyncio.run(main())
